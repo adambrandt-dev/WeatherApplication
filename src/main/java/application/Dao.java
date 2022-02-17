422beb56fd734a9904c78ece8ba7730d8d1b598e
Adam <kobra.brandt@gmail.com>
2022-02-17 19:08:30 +0100

    Creation of interface Dao with class DaoImpl.

---
+++ src/main/java/application/Dao.java
@@ -0,0 +1,15 @@
+package application;
+
+public interface Dao {
+
+    void checkChoiceBox();
+    void createTable();
+    void searchBy();
+    void addNew();
+    void clearField();
+    void switchToStartScene();
+    void switchToLocationScene();
+    void switchToLocationShowScene();
+    void switchToWeatherScene();
+    void exit();
+}
