75d95a24fb8ef0a502b8badb12810e654b44105b
Adam <kobra.brandt@gmail.com>
2022-02-23 18:13:23 +0100

    First try to make dynamical table creation work

--- src/main/java/application/StartSceneController.java
+++ src/main/java/application/StartSceneController.java
@@ -1,24 +1,6 @@
 package application;
 
-import javafx.event.ActionEvent;
-import java.io.IOException;
 
 public class StartSceneController extends DaoImpl{
-
-    @Override
-    public void switchToLocationShowScene(ActionEvent event) throws IOException {
-        super.switchToLocationShowScene(event);
-    }
-
-    @Override
-    public void switchToWeatherScene(ActionEvent event) throws IOException {
-        super.switchToWeatherScene(event);
-    }
-
-    @Override
-    public void exit(ActionEvent event) {
-        super.exit(event);
-    }
-
 
 }
